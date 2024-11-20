6a1659e21cc8396586b5d36872f9d71ab1860793
ifiyemiO <ifiyemi.owi@keyin.com>
2024-11-20 19:36:28 -0330

    models update

---
+++ src/main/java/com.foodorder/models/Order.java
@@ -0,0 +1,18 @@
+package com.foodorder.models;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    // Other fields, getters, and setters
+}
+
---
+++ src/main/java/com.foodorder/models/Restaurant.java
@@ -0,0 +1,29 @@
+package com.foodorder.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Restaurant {
+
+    @Id
+    private Long id;
+    private String name;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
